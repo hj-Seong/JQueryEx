2cb8651336dc57b624e228eca0254f8f62512db2
hj <hjseong1222@gamil.com>
2022-03-13 22:43:32 +0900

    JQuery 프로젝트

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+	// 슬릭 추가 및 속성 변경
+	/* 직접 작성할 공간 */
+
+
+
+	// 탭 메뉴 숨기기 및 활성화
+	$("#notice_tab_wrap h4 a").on("click", tabmenu);
+	function tabmenu(e) {
+		/* 직접 작성할 공간 */
+		e.preventDefault();
+
+	}
+
+});
